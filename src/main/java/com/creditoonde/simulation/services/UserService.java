bbb162d239b4970a4b8edc8234b1ec1b3702a679
Carlos Baumont <baumont.carlos@gmail.com>
2021-04-03 12:57:25 -0300

    mongodb com repository e service

---
+++ src/main/java/com/creditoonde/simulation/services/UserService.java
@@ -0,0 +1,19 @@
+package com.creditoonde.simulation.services;
+
+import com.creditoonde.simulation.domain.User;
+import com.creditoonde.simulation.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
